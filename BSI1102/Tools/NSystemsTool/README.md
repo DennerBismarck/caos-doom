b1910c46c31f00c25305afd2c709111d42fc1ecc
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2024-03-15 08:47:52 -0300

    Upgrading repository directories

---
+++ BSI1102/Tools/NSystemsTool/README.md
@@ -0,0 +1,12 @@
+# Number Systems Tool
+
+This project has the objective to create a software that initially is capable of:
+
+1. Handling multiple basis conversions and arithmetics
+2. Producing LaTeX align structures that simulate "as in-school" arithmetic
+3. Producing interactive ways for students to grasp different basis arithmetic
+
+Future ideas include:
+
+1. Making it a non-terminal software with proper presentation
+2. 
---
+++ BSI1102/Tools/NSystemsTool/main.py
@@ -0,0 +1,30 @@
+class Number:
+    
+    def __init__(self):
+        self.base = None
+        self.d_string = None
+        self.value = None
+        self.dec_value = None
+        self.c_value = None
+
+class Base:
+
+    def __init__(self):
+        self.base = None
+        self.digits = None
+
+class Digit:
+
+    def __init__(self):
+        self.symbol = None
+        self.value = None
+
+class Carry:
+
+    def __init__(self):
+
+class Arithmetic:
+
+    def __init__(self):
+
+
